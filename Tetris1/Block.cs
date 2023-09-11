d20a3ac7b9febe0a7bf0cc72ca8fab510bc35232
öric <ric@DESKTOP-74ICC92>
2023-09-11 23:33:51 +0200

    initial commit

---
+++ Tetris1/Block.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Tetris1
+{
+    /*
+     * Abstract class which contains the Data and Form of Blocks aswell as their rotation state and offset
+     * Moves and rotates the blocks
+     */
+    public abstract class Block
+    {
+        protected abstract BlockPosition[][] Tiles { get; } //Sets the Form of the block in all 4 Rotations
+        protected abstract BlockPosition StartOffset { get; }
+        public abstract int Id { get; }
+        private int rotationState;
+        private BlockPosition offset;
+
+        public Block()
+        {
+            offset = new BlockPosition(StartOffset.Row, StartOffset.Column);
+
+        }
+
+
+        //Returns the grid position occupied by the block while taking the rotation into account
+        public IEnumerable<BlockPosition> TilePositions()
+        {
+            foreach (BlockPosition bp in Tiles[rotationState])
+            {
+                yield return new BlockPosition(bp.Row + offset.Row, bp.Column + offset.Column);
+            }
+        }
+
+        public void RotateClockWise()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotatteCounterClockWise()
+        {
+            if (rotationState == 0)
+                rotationState = Tiles.Length - 1;
+            else
+                rotationState--;
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+
+    }
+}
---
+++ Tetris1/BlockQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+/*
+ * Class that generates a new block that is placed after the first block finishes
+ */
+namespace Tetris1
+{
+    public class BlockQueue
+    {
+
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random rdm = new();
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+        private Block RandomBlock()
+        {
+            return blocks[rdm.Next(blocks.Length)];
+        }
+
+        //adds a different block into the queue
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+            return block;
+        }
+
+    }
+}
